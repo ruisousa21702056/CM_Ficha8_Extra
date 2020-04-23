4e1f547b3a2a0e14373d8af80581d4ff97b7ea1c
Rui Sousa <rp.sousa91@gmail.com>
2020-04-23 17:56:06 +0100

    Feito o logout. Continua a faltar manter users e mostrar nome e email no drawer

--- app/src/main/java/com/example/acalculator/MainActivity.kt
+++ app/src/main/java/com/example/acalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.acalculator
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -35,9 +36,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.nav_calculator -> NavigationManager.goToCalculatorFragment(supportFragmentManager)
-            R.id.nav_history -> NavigationManager.goToHistoryFragment(supportFragmentManager)
+        when (item.itemId) {
+            R.id.nav_calculator -> {
+                NavigationManager.goToCalculatorFragment(supportFragmentManager)
+            }
+            R.id.nav_history -> {
+                NavigationManager.goToHistoryFragment(supportFragmentManager)
+            }
+            R.id.nav_logout -> {
+                val logoutIntent = Intent(this,  LoginActivity::class.java)
+                startActivity(logoutIntent)
+                finish()
+            }
         }
         drawer.closeDrawer(GravityCompat.START)
         return true
